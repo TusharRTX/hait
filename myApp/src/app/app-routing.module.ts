e66c3a266d9127fb4886c22a016b41515d8db48c
Tushar Mirwani <tu.mirwani@duocuc.cl>
2024-05-17 11:49:59 -0400

    v2 chao
    v2 chao

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -27,9 +27,6 @@ const routes: Routes = [
     path: 'registro',
     loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
   },
-  { path: 'bancochile', 
-    loadChildren: () => import('./bancochile/bancochile.module').then(m => m.BancochilePageModule) 
-  },
 
 
 ];
