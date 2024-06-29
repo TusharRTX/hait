c3168784e5c25ef0eff17fbe5197cf2dca565822
Tushar Mirwani <tu.mirwani@duocuc.cl>
2024-06-29 00:04:50 -0400

    listo pedidos
    listo pedidos

--- myApp/src/app/pedidos/pedidos.page.ts
+++ myApp/src/app/pedidos/pedidos.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DjangoapiService } from '../conexion/djangoapi.service';
+import { CartService } from '../services/cart.service';
+import { MenuController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';  
 
 @Component({
   selector: 'app-pedidos',
@@ -8,10 +13,20 @@ import { DjangoapiService } from '../conexion/djangoapi.service';
 })
 export class PedidosPage implements OnInit {
   pedidos: any[] = [];
+  isDropdownOpen = false;
+  isAuthenticated: boolean = false;
+  role: string = '';
 
-  constructor(private djangoApiService: DjangoapiService) {}
+  constructor(private alertController: AlertController,private router: Router,private toastController: ToastController, private cartService: CartService,private menu: MenuController,private djangoApiService: DjangoapiService) {}
 
   ngOnInit() {
+    this.djangoApiService.isAuthenticated$.subscribe(isAuth => {
+      this.isAuthenticated = isAuth;
+    });
+    this.djangoApiService.role$.subscribe(role => {
+      this.role = role;
+    });
+
     console.log('Initializing PedidosPage...');
     this.djangoApiService.getPedidos().subscribe(
       (data) => {
@@ -23,5 +38,44 @@ export class PedidosPage implements OnInit {
       }
     );
   }
+
+  async presentLogoutAlert() {
+    const alert = await this.alertController.create({
+      header: 'Cerrar sesión',
+      message: '¿Estás seguro de que deseas cerrar sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Sí',
+          handler: async () => {
+            await this.djangoApiService.logout();
+            this.router.navigate(['/home']);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  toggleDropdown(open: boolean) {
+    this.isDropdownOpen = open;
+    const dropdown = document.getElementById('dropdown-menu');
+    const button = document.getElementById('products-category-button');
+    if (dropdown && button) {
+      if (open) {
+        const rect = button.getBoundingClientRect();
+        dropdown.style.top = `${rect.bottom}px`; // adjust positioning
+        dropdown.style.left = `${rect.left}px`;
+        dropdown.style.display = 'block';
+      } else {
+        dropdown.style.display = 'none';
+      }
+    }
+  }
+
 }
 
