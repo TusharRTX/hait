9337d5cfed05c36fa93b3c4e89e7f4de29db2cc3
Tushar Mirwani <tu.mirwani@duocuc.cl>
2024-07-11 21:51:05 -0400

    listo contador
    listo contador

--- myApp/src/app/voucher/voucher.page.ts
+++ myApp/src/app/voucher/voucher.page.ts
@@ -17,6 +17,7 @@ export class VoucherPage implements OnInit {
   isDropdownOpen = false;
   isAuthenticated: boolean = false;
   role: string = '';
+  isLoading: boolean = false;
 
   constructor(private alertController: AlertController,private router: Router,private toastController: ToastController,private apiService: DjangoapiService,private menu: MenuController) {}
 
@@ -34,24 +35,45 @@ export class VoucherPage implements OnInit {
 
   fetchVouchers() {
     if (this.selectedDate) {
-      const formattedDate = new Date(this.selectedDate).toISOString().split('T')[0]; // Formatea la fecha a YYYY-MM-DD
+      this.isLoading = true;  
+      const formattedDate = new Date(this.selectedDate).toISOString().split('T')[0];  // Formatea la fecha a YYYY-MM-DD
       this.apiService.getVouchers(formattedDate).subscribe(
-        response => {
+        (response) => {
           this.vouchers = response;
           this.vouchers.forEach(voucher => {
             if (voucher.enviado_info && voucher.enviado_info.enviado) {
               voucher.enviado_info.enviado_at_formatted = this.formatDate(voucher.enviado_info.enviado_at);
             }
           });
+          this.isLoading = false;  
         },
-        error => {
+        (error) => {
           console.error('Error fetching vouchers:', error);
+          this.isLoading = false; 
         }
       );
     }
   }
 
   enviarCorreo(voucher: any) {
+    const email = voucher.user.correo;
+    const subject = `Comprobante de venta ${voucher.voucher_id}`;
+    const body = `
+  Comprobante de Venta: ${voucher.voucher_id}
+  Emitido por: ${voucher.user.nombre} ${voucher.user.apellido}
+  Total: ${voucher.total}
+  Fecha: ${new Date(voucher.created_at).toLocaleDateString()}
+  Items:
+  ${voucher.items.map((item: any) => `${item.title} - Cantidad: ${item.quantity} - Precio Unitario: ${item.unit_price}`).join('\n')}
+  No valido como boleta
+  
+  Logo:
+  https://media.discordapp.net/attachments/968742341613596692/1241022234265518242/logo2_preview_rev_1.png?ex=6691dac5&is=66908945&hm=7721e602804f06b3ca77a9277659592eaa14e4c196db80096ce86f2c4b2438a8&=&format=webp&quality=lossless
+  `;
+  
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = mailtoLink;
+
     this.apiService.markVoucherAsSent(voucher.voucher_id).subscribe(
       response => {
         voucher.enviado_info.enviado = true;
